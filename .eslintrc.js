5932cc804a1e57867a1b6c7d93087f8b94eca80d
zahra-99 <zahraahmed6@hotmail.co.uk>
2021-11-03 14:47:09 +0000

    created eslint formatter

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "airbnb-base", "eslint:recommended"
+    ],
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"]
+    }
+};
